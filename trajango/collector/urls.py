dbd653ef1aa5ea5723c7b9fb3217352143bf7e0b
Bilal Tonga <btonga@d80e17f36f23>
2020-04-20 16:22:31 +0300

    csrf token except

--- trajango/collector/urls.py
+++ trajango/collector/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from . import views
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', csrf_exempt(views.index), name='index'),
 ]
